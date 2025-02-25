b861b336038d0d01483c7f71f0058a03134793e6
JunasVee <87957663+JunasVee@users.noreply.github.com>
2025-02-25 19:52:39 +0700

    Update apiClient.ts

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const apiClient = axios.create({
-    baseURL: 'http://91.108.111.175:5000/api/v1/',
+    baseURL: 'https://api.dynamits.id/api/v1/',
     headers: {
         'Content-Type': 'application/json',
     },
